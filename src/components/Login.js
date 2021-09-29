0799bf2750cb9724e31622653e7b576ffaeb6a71
FREEDOM <freewhynane62@gmail.com>
2021-09-29 12:14:12 +0000

    add buttons

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,10 +3,21 @@ import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
 
 const Login = () => {
     return (
-        <div>
-            
+        <div id="login-page">
+            <div id="login-card">
+                <h2>Welcome to Chatroom!</h2>
+
+                <div className="login-button google">
+                    <GoogleOutlined /> Sign In With Google
+                </div>
+                <br/> <br />
+
+                <div className="login-button facebook">
+                    <FacebookOutlined /> Sign In With Facebook
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Login
