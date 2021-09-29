70dd30268d4d082c6e66705af39f76a34cc1c404
FREEDOM <freewhynane62@gmail.com>
2021-09-29 10:42:16 +0000

    add login component

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
+
+const Login = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Login
